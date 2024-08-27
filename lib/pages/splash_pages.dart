811395ecf509af331b25f1b11222134a71a1b615
hanis siddiq <hanissiddiq10@gmail.com>
2024-08-28 03:42:23 +0700

    add splash screen

---
+++ lib/pages/splash_pages.dart
@@ -0,0 +1,44 @@
+import "dart:async";
+
+import "package:flutter/material.dart";
+import "package:apotik_apps/theme.dart";
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.pushNamed(context, '/sign-in'),
+    );
+    // initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: aColor,
+      body: Center(
+        child: Container(
+          width: 92,
+          height: 120,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/images/apotik_logo.png',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
